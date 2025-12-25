31c0bf1bb0c6f678e2c49e851aaf66adf66d2c73
Gedeon Tiga <gedeontigadev@gmail.com>
2025-12-25 20:46:24 +0100

    Fix Swagger config to properly fetch definitions from  downstream services

--- api-gateway/src/main/java/com/facsciencesuy1/planning_management/api_gateway/utils/configs/SwaggerConfig.java
+++ api-gateway/src/main/java/com/facsciencesuy1/planning_management/api_gateway/utils/configs/SwaggerConfig.java
@@ -6,11 +6,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -28,13 +25,6 @@ public class SwaggerConfig {
 						.license(new License()
 								.name("Apache 2.0")
 								.url("https://www.apache.org/licenses/LICENSE-2.0.html")))
-				.servers(List.of(
-						new Server()
-								.url("http://localhost:8080")
-								.description("Local Gateway"),
-						new Server()
-								.url("https://api.example.com")
-								.description("Production Gateway")))
 				.components(new io.swagger.v3.oas.models.Components()
 						.addSecuritySchemes("bearer-jwt", new SecurityScheme()
 								.type(SecurityScheme.Type.HTTP)
--- planning-service/src/main/java/com/facsciencesuy1/planning_management/planning_service/utils/configs/SwaggerConfig.java
+++ planning-service/src/main/java/com/facsciencesuy1/planning_management/planning_service/utils/configs/SwaggerConfig.java
@@ -7,11 +7,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -30,13 +27,6 @@ public class SwaggerConfig {
 						.license(new License()
 								.name("Apache 2.0")
 								.url("https://www.apache.org/licenses/LICENSE-2.0.html")))
-				.servers(List.of(
-						new Server()
-								.url("http://localhost:8080")
-								.description("Direct Service Access (Dev Only)"),
-						new Server()
-								.url("http://api-gateway:8080") // Gateway port
-								.description("API Gateway (Production)")))
 				.components(new Components()
 						.addSecuritySchemes("gateway-auth", new SecurityScheme()
 								.type(SecurityScheme.Type.APIKEY)
